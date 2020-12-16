d18e6ebf9d0f2704dff9898e7de74a4e3875490e
“Julia <juliavasastan@gmail.com>
2020-12-16 21:22:23 +0100

    Create home screen components

--- src/screens/homeScreen.js
+++ src/screens/homeScreen.js
@@ -4,13 +4,24 @@ import {
     Text,
 } from 'react-native';
 
+import ScreenTitle from '../components/screenTitle';
+import StandardButton from '../components/standardButton';
+
 
 // Empty HomeScreen.
 
+
+
+
 export default function HomeScreen({ navigation }) {
+
+
+
     return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text>HomeScreen</Text>
+        <View style={{ flex: 1, alignItems: "center", justifyContent: 'center' }}>
+            <ScreenTitle title="City Pop" />
+            <StandardButton text="SEARCH BY CITY" onPress={() => navigation.push("SearchByCity")} />
+            <StandardButton text="SEARCH BY COUNTRY" onPress={() => navigation.push("SearchByCountry")} />
         </View>
     );
 }
