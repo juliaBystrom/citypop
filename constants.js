20b604f27551bc26f858a78a4443954fca72146c
“Julia <juliavasastan@gmail.com>
2020-12-17 17:37:27 +0100

    Add base url as constant

---
+++ constants.js
@@ -0,0 +1,16 @@
+export const BASEURL = 'http://api.geonames.org/searchJSON?';
+
+
+
+/*
+
+body: JSON.stringify({
+                firstParam: 'yourValue',
+                secondParam: 'yourOtherValue',
+            }),
+
+
+
+
+             body: JSON.stringify(data)
+*/
