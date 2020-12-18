80721bc59964094b024237a3e9cfce2e768ad398
“Julia <juliavasastan@gmail.com>
2020-12-19 00:06:27 +0100

    Implemented fetching country and then cities

--- src/screens/searchByCoutryScreen.js
+++ src/screens/searchByCoutryScreen.js
@@ -2,11 +2,12 @@ import React, { useReducer, useEffect } from 'react';
 import {
     View,
     Text,
+    Button,
 } from 'react-native';
 import ScreenTitle from '../components/screenTitle';
 import UserStringInput from '../components/userStringInput';
 import UtilAPI from '../utils/utilAPI';
-import { BASEURL } from '../../constants';
+import { BASEURL, BASEURLCONTAINS } from '../../constants';
 import { USERNAME } from '../../credentials';
 import useDidMount from '../utils/useDidMount';
 
@@ -23,6 +24,7 @@ const initialState = {
         { city: '', population: 0 },
         { city: '', population: 0 }
     ],
+    countryCode: 0,
 
 }
 function reducer(state, action) {
@@ -30,7 +32,7 @@ function reducer(state, action) {
         case 'fieldChange': {
             return {
                 ...state,
-                [action.country]: action.payload
+                [action.fieldName]: action.payload
 
             };
         }
@@ -56,7 +58,12 @@ function reducer(state, action) {
                 error: action.errorMessage,
             }
 
-
+        }
+        case 'updateId': {
+            return {
+                ...state,
+                countryCode: action.countryCode,
+            }
         }
         default:
             return state;
@@ -66,16 +73,30 @@ function reducer(state, action) {
 
 export default function SearchByCountryScreen({ navigation }) {
     const [state, dispatch] = useReducer(reducer, initialState)
-    const { country, isLoading, error, displayCountry, cities } = state;
+    const { country, isLoading, error, displayCountry, cities, countryCode } = state;
 
     // False if the component is just being rendered and inserted into dom, true if not
     const didMount = useDidMount();
 
-    // data parameters to be sent 
-    var data = {
+
+    // Data parameters to be sent when finding countryCode of country searched for
+    var dataFindCountry = {
         'name': country,
-        'featureClass': 'P',
+        'name_equals': country,
+        'featureClass': 'A',
         'isNameRequired': 'true',
+        'username': USERNAME,
+        'type': 'json',
+        'orderby': 'relevance',
+        'maxRows': '1',
+    };
+
+
+    // Data parameters to be sent when finding largest cities of country searched for
+    var dataFindCities = {
+        'p': country,
+        'country': countryCode,
+        'featureClass': 'P',
         'username': USERNAME,
         'type': 'json',
         'orderby': 'population',
@@ -85,19 +106,33 @@ export default function SearchByCountryScreen({ navigation }) {
     // Function that handles when a user search fo a country
     const pressHandler = () => {
         dispatch({ type: 'search' });
-        UtilAPI({ baseURL: BASEURL, data: data, onSuccess: successSearch, onError: errorSearch });
+        UtilAPI({ baseURL: BASEURL, data: dataFindCountry, onSuccess: successSearchCountry, onError: errorSearch });
 
     }
+
+    const successSearchCountry = ({ responseJson }) => {
+        // Will return a list of only the name´s of cities 
+        const countryCode = responseJson.geonames[0].countryCode;
+        dispatch({ type: 'updateId', countryCode: countryCode });
+
+    }
+
+    // When the countryCode is changed this userhock will call UtilAPI to search for largest city within that country
+    useEffect(() => {
+        if (didMount) {
+            UtilAPI({ baseURL: BASEURL, data: dataFindCities, onSuccess: successSearch, onError: errorSearch });
+        }
+
+    }, [countryCode]
+    )
 
     // Function handles when the search query successed
     // Parameters is country name to display with up to 3 of the most populated cities in that country
     const successSearch = ({ responseJson }) => {
         // Will return a list of only the name´s of cities 
-        const citiesFound = responseJson.map((geonames) => {
-            return { city: geonames.name, population: geonames.population };
+        const citiesFound = responseJson.geonames.map((entry) => {
+            return { city: entry.name, population: entry.population };
         });
-
-        console.log(citiesFound);
 
         dispatch({
             type: 'success',
@@ -124,6 +159,7 @@ export default function SearchByCountryScreen({ navigation }) {
     const errorSearch = (errorMessage) => {
         dispatch({ type: 'error', errorMessage: errorMessage });
     }
+
 
 
     return (
