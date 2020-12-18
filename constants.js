0f05dd0723657895d00db3b9c8d3d3b33d190ac4
“Julia <juliavasastan@gmail.com>
2020-12-18 19:53:45 +0100

    Add Unknown error message

--- constants.js
+++ constants.js
@@ -1,17 +1,2 @@
 export const BASEURL = 'http://api.geonames.org/searchJSON?';
-export const ERROR_MESSAGE = {serverError: 'Internal server error while loading data, please waint and try again later', clientError: 'Bad request error, please contact app administrato'};
-
-
-
-/*
-
-body: JSON.stringify({
-                firstParam: 'yourValue',
-                secondParam: 'yourOtherValue',
-            }),
-
-
-
-
-             body: JSON.stringify(data)
-*/
+export const ERROR_MESSAGE = {serverError: 'Internal server error while loading data, please waint and try again later.', clientError: 'Bad request error, please contact app administrato', unknownError: 'Unknown error,, please waint and try again later.', clientError: 'Bad request error, please contact app administrato.'};
