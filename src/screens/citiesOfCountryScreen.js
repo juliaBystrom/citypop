d469bb9ea5a879cada7e1ca54511bb96ea79c8d2
“Julia <juliavasastan@gmail.com>
2020-12-19 00:07:13 +0100

    Implement templete list for citites

--- src/screens/citiesOfCountryScreen.js
+++ src/screens/citiesOfCountryScreen.js
@@ -2,15 +2,26 @@ import React from 'react';
 import {
     View,
     Text,
+    FlatList,
 } from 'react-native';
+import ScreenTitle from '../components/screenTitle';
 
 
 // Empty CitiesOfCountryScreen.
 
-export default function CitiesOfCountryScreen({ navigation }) {
+export default function CitiesOfCountryScreen({ route, navigation }) {
+    const { displayCountry, topCities } = route.params;
+
     return (
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text>Cities Of Country Screen</Text>
+            <ScreenTitle title={displayCountry} />
+            <FlatList
+                numColumns={1}
+                keyExtractor={(entry) => entry.city}
+                data={topCities}
+                renderItem={({ item }) => (
+                    <Text>-</Text>
+                )} />
         </View>
     );
 }
