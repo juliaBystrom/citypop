3d2ef62fc1014473c0f391d8b93fc2f20f385dfb
“Julia <juliavasastan@gmail.com>
2020-12-18 19:49:57 +0100

    Add standard error messages

--- constants.js
+++ constants.js
@@ -1,4 +1,5 @@
 export const BASEURL = 'http://api.geonames.org/searchJSON?';
+export const ERROR_MESSAGE = {serverError: 'Internal server error while loading data, please waint and try again later', clientError: 'Bad request error, please contact app administrato'};
 
 
 
