f283d4416e3efcf4cc424b3c0501507e72967ffd
“Julia <juliavasastan@gmail.com>
2020-12-19 13:49:33 +0100

    Add index.js file for readability

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+import PopulationDisplay from './populationDisplay';
+import ScreenTitle from './screenTitle';
+import StandardButton from './standardButton';
+import UserStringInput from './userStringInput';
+import PressableCity from './pressableCity';
+
+export { PopulationDisplay, ScreenTitle, StandardButton,UserStringInput, PressableCity };
