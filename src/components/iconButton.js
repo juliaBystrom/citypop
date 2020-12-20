86ad026aec8a59c4150cab43826a2eb4b50511e9
“Julia <juliavasastan@gmail.com>
2020-12-20 22:19:58 +0100

    Add basic Icon button component

---
+++ src/components/iconButton.js
@@ -0,0 +1,23 @@
+/*
+*  Button component with icon 
+*  Input props: 
+*  Name of icon to be displayed, see ICONS constants.js for availiable names
+*  onPress function to be executed when the button is pressed
+*/
+
+import React from 'react';
+import {
+    Pressable,
+    Text,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+
+export default function IconButton(iconName, onPress) {
+    
+    return (
+        <Pressable onPress={onPress}>
+            <Icon name={iconName} size={30} color="#900" />
+        </Pressable>
+    );
+}
--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,6 @@ import ScreenTitle from './screenTitle';
 import StandardButton from './standardButton';
 import UserStringInput from './userStringInput';
 import PressableCity from './pressableCity';
+import IconButton from './iconButton';
 
-export { PopulationDisplay, ScreenTitle, StandardButton, UserStringInput, PressableCity };
+export { PopulationDisplay, ScreenTitle, StandardButton, UserStringInput, PressableCity, IconButton };
